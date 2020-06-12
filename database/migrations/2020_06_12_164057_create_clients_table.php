5dcce55faa6b341fb2b0f58742705cd57d216023
fabioMatosCosta <fabiodanielmc@gmail.com>
2020-06-12 17:41:05 +0200

    first commit

---
+++ database/migrations/2020_06_12_164057_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 150)->comment('Client name');
+            $table->string('image', 100)->nullable()->comment('Photo field');
+            $table->string('cpf_cnpj', 19)->unique()->comment('cpf ou cnpj of the client');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
