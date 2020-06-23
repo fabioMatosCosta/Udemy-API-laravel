6e038f582d044dec2aaf57a2de71b03c56ecf436
fabioMatosCosta <fabiodanielmc@gmail.com>
2020-06-23 11:24:47 +0200

    install intervention\image and file upload (not done yet)

--- app/Http/Controllers/Api/ClientApiController.php
+++ app/Http/Controllers/Api/ClientApiController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Client;
-
+use Illuminate\Validation\Rules\Unique;
+use Intervention\Image\ImageManagerStatic as Image;
+use Illuminate\Support\Facades\Storage;
 
 class ClientApiController extends Controller
 {
@@ -18,7 +20,7 @@ class ClientApiController extends Controller
 
     public function index()
     {
-        $data = $this->client->all();
+        $data = $this->client->all();   
        
         return response()->json($data);
     }
@@ -31,6 +33,22 @@ class ClientApiController extends Controller
         $dataForm = $request->all();
         
         $data = $this->client->create($dataForm);
+
+        if($request->hasFile('image') && $request->file('image')->isValid()){
+            $extension = $request->image->extension();
+
+            $name = uniqid(date('His'));
+
+            $nameFile = `{$name}.{$extension}`;
+
+            $upload = Image::make($dataForm['image'])->resize(177, 236)->save(storage_path(`app/public/clients/$nameFile`, 70));
+
+            if(!$upload){
+                return response()->json(['error'=>'Upload failed'], 500);
+            }else{
+                $dataForm['image'] = $nameFile; 
+            }
+        }
 
         return response()->json($data, 201);
     }
